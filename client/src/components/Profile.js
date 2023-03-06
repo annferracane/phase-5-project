9b00f96073a16f638ec60c6e80e63091a32cc775
Ann Ferracane <ann@patchventures.com>
2023-03-06 16:52:32 -0500

    fixed fallback route

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useContext } from "react";
+import { useState, useContext } from "react";
 import { UserContext } from "../context/user";
 import { useHistory } from 'react-router-dom';
 import ActionAlerts from './ActionAlerts';
