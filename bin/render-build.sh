a67aa6128e60ca2e1968cfd35fb86637c3cd0e83
Ann Ferracane <ann@patchventures.com>
2023-03-07 13:36:32 -0500

    bin rake file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -9,5 +9,7 @@ cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
+bundle exec rails db:drop
+bundle exec rails db:create
 bundle exec rails db:migrate 
 bundle exec rails db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
