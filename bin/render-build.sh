29f6b90e565e90cffcb0dc08df8f41d731612d44
Ann Ferracane <ann@patchventures.com>
2023-03-07 13:46:59 -0500

    changed the render file

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,12 +4,10 @@ set -o errexit
 
 # Build commands for front end to create the production build
 rm -rf public
-npm install --prefix client && npm run build --prefix client
+npm install --prefix client && npm run build:production --prefix client
 cp -a client/build/. public/
 
 # Build commands for back end
 bundle install
-bundle exec rails db:drop
-bundle exec rails db:create
 bundle exec rails db:migrate 
 bundle exec rails db:seed # if you have seed data, run this command for the initial deploy only to avoid duplicate records
